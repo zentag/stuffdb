e41305543decdd220f6fbdf1b81fedb43f15a606
zentag <38821230+zentag@users.noreply.github.com>
2025-02-12 21:12:51 -0500

    add vite sass

--- svelte-app/src/vite-env.d.ts
+++ svelte-app/src/vite-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import sass from "vite-plugin-sass";
+
+export default defineConfig({
+  plugins: [sass({ sassOptions: { indentedSyntax: true } })],
+});
