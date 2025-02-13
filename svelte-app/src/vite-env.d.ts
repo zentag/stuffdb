5edce0cf166a2cb26b41dcc3e020e68dd9bb53e7
zentag <38821230+zentag@users.noreply.github.com>
2025-02-12 21:15:35 -0500

    remove vite sass bs

--- svelte-app/src/vite-env.d.ts
+++ svelte-app/src/vite-env.d.ts
@@ -1,8 +1,2 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-import { defineConfig } from "vite";
-import sass from "vite-plugin-sass";
-
-export default defineConfig({
-  plugins: [sass({ sassOptions: { indentedSyntax: true } })],
-});
