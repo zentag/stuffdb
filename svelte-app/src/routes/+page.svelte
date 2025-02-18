1de27239100b4e641bc0f48317b4a7687a689ef3
zentag <38821230+zentag@users.noreply.github.com>
2025-02-18 12:40:45 -0500

    add css

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { auth, supabase } from "$lib/supabase";
 	import Login from "$lib/components/login.svelte";
-	const defaultTableSelect = "Select thing type...";
 	let selectedTable = $state("");
 	let newTableName = $state("");
 	async function newTable() {
@@ -12,58 +11,83 @@
 	}
 </script>
 
-<h1>StuffDB</h1>
 {#await auth.getUser() then user}
 	{#if user.data.user}
 		{#await supabase.rpc("list_tables" + selectedTable.slice(0, 0)) then tables}
-			<select
-				name="select"
-				aria-label="select thing type..."
-				bind:value={selectedTable}
-			>
-				<option selected disabled value="">select thing type... </option>
-				{#each tables.data as table}
-					<option>
-						{table.tablename.split("_").join(" ")}
-					</option>
-				{/each}
-				<option>New thing type</option>
-			</select>
+			<div class="header">
+				<h1>StuffDB</h1>
+				<select
+					name="select"
+					aria-label="select thing type..."
+					bind:value={selectedTable}
+				>
+					<option selected disabled value="">all</option>
+					{#each tables.data as table}
+						<option>
+							{table.tablename.split("_").join(" ")}
+						</option>
+					{/each}
+					<option>New thing type</option>
+				</select>
+			</div>
 		{/await}
 		{#await supabase.rpc("get_cols", { tablename: selectedTable }) then table}
 			<div id="options">
 				{#each table.data as column}
 					{#if column.type === "smallint"}
-						<input type="number" placeholder={column.name + ": min"} />
-						<input type="number" placeholder={column.name + ": max"} />
+						<input
+							type="number"
+							placeholder={column.name + ": min"}
+							class="number"
+						/>
+						<input
+							type="number"
+							placeholder={column.name + ": max"}
+							class="number"
+						/>
 					{:else if column.type === "text" && column.name !== "name"}
-						<select name={column.name} aria-label={column.name}>
-							<option selected disabled value="">{column.name}</option>
-							{#await supabase
-								.from(selectedTable)
-								.select(column.name) then values}
-								{#each values.data as value}
-									<option>{value[column.name]}</option>
-								{/each}
-							{/await}
-						</select>
+						<details class="dropdown">
+							<summary>{column.name}</summary>
+							<ul>
+								{#await supabase
+									.from(selectedTable)
+									.select(column.name) then values}
+									{#each values.data as value}
+										<li>
+											<label class="checkboxlabel">
+												<input
+													type="checkbox"
+													name={value[column.name]}
+													class="checkbox"
+												/>
+												<p class="checkboxlabel">{value[column.name]}</p></label
+											>
+										</li>
+									{/each}
+								{/await}
+							</ul>
+						</details>
 					{:else if column.name === "id"}{:else}
 						<input type="text" placeholder={column.name} />
 					{/if}
 				{/each}
+				{#if table.data[0]}
+					<button>New column</button>
+				{/if}
 			</div>
-			{#if table.data[0]}
-				<button>New column</button>
-			{/if}
 		{/await}
+		{#if selectedTable !== "New thing type"}
+			<input placeholder="search..." class="search" />
+		{/if}
 	{:else}
+		<h1>StuffDB</h1>
 		<Login />
 	{/if}
 {/await}
 {#if selectedTable == "New thing type"}
 	<form onsubmit={newTable}>
-		<label>
-			Name of thing type:
+		<label class="label">
+			<p>Name of thing type:</p>
 			<input type="text" bind:value={newTableName} />
 		</label>
 		<button>Submit</button>
@@ -74,4 +98,28 @@
 #options
   display: flex 
   flex-direction: row
+  flex-columns: 2
+  flex-wrap: wrap
+input[type=number], input[type=text], select
+  width:12rem
+button 
+  height: 3.18rem
+p:not(.checkboxlabel), select, input:not([type=checkbox]), button, h1, details
+  margin-top: 1rem
+  margin-left: 1rem
+.header 
+  display: flex
+  align-items: center
+.search 
+  width: 21.5rem
+.label 
+  display: flex
+  flex-direction: column
+label > p 
+  margin-bottom: -.5rem
+label.checkboxlabel 
+  display: flex
+  align-items: center
+input[type=checkbox]
+  margin-top: .4rem
 </style>
