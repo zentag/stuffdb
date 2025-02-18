143a7d86d28aabc8fe3743f5aa5992d565376da2
zentag <38821230+zentag@users.noreply.github.com>
2025-02-17 19:39:13 -0500

    add create table

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,23 +1,46 @@
 <script lang="ts">
 	import { auth, supabase } from "$lib/supabase";
 	import Login from "$lib/components/login.svelte";
-	let selectedTable = $state("Select component...");
+	const defaultTableSelect = "Select thing type...";
+	let selectedTable = $state(defaultTableSelect);
+	let newTableName = $state("");
+	async function newTable() {
+		await supabase.rpc("create_table", {
+			name: newTableName,
+		});
+		selectedTable = defaultTableSelect;
+	}
 </script>
 
 <h1>StuffDB</h1>
 {#await auth.getUser() then user}
 	{#if user.data.user}
 		{#await supabase.rpc("list_tables") then tables}
-			<select name="select" aria-label="Select" required value={selectedTable}>
-				<option>{selectedTable}</option>
+			<select
+				name="select"
+				aria-label="Select"
+				required
+				bind:value={selectedTable}
+			>
+				<option>{defaultTableSelect}</option>
 				{#each tables.data as table}
 					<option>
-						{table.tablename}
+						{table.tablename.split("_").join(" ")}
 					</option>
 				{/each}
+				<option>+ New thing type</option>
 			</select>
 		{/await}
 	{:else}
 		<Login />
 	{/if}
 {/await}
+{#if selectedTable == "+ New thing type"}
+	<form onsubmit={newTable}>
+		<label>
+			Name of thing type:
+			<input type="text" bind:value={newTableName} />
+		</label>
+		<button>Submit</button>
+	</form>
+{/if}
