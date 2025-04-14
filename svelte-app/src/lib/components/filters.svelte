dab41449a82f5fcde4de4ca30d4252e3f9569be5
zentag <38821230+zentag@users.noreply.github.com>
2025-04-14 15:46:07 -0400

    separate filters

---
+++ svelte-app/src/lib/components/filters.svelte
@@ -0,0 +1,150 @@
+<script lang="ts">
+	import { supabase } from "$lib/supabase";
+
+	let filters = $state({});
+	let { table, algoliaFilters = $bindable(), selectedTable } = $props();
+
+	$effect(() => {
+		let tempFilters = "";
+		Object.keys(filters).forEach(async (key) => {
+			const filter = filters[key];
+			if (filter[0]) {
+				tempFilters += `${key} >= ${filter[0]} AND `;
+			}
+			if (filter[1]) {
+				tempFilters += `${key} <= ${filter[1]} AND `;
+			}
+			// text filter
+			if (filter.constructor === Object) {
+				Object.keys(filter).forEach((k) => {
+					if (filter[k]) tempFilters += `${key}:'${k}' OR `;
+				});
+			}
+		});
+		// workaround to bypass the OR statement from text statements
+		if (selectedTable) {
+			if (tempFilters.split("OR").pop() == " ")
+				tempFilters = tempFilters.slice(0, -3) + "AND ";
+			tempFilters += `table: ${selectedTable}`;
+			algoliaFilters = tempFilters;
+		} else algoliaFilters.filters = "";
+	});
+	$inspect(algoliaFilters);
+</script>
+
+<div id="options">
+	{#each table.data as column}
+		{#if column.type === "smallint"}
+			{#if filters[column.name]?.[2]}
+				<input
+					type="number"
+					placeholder={column.name + ": min"}
+					class="number"
+					oninput={(e) => {
+						filters[column.name] = [
+							e.target.value,
+							filters[column.name]?.[1] || null,
+							true,
+						];
+					}}
+				/>
+				<input
+					type="number"
+					placeholder={column.name + ": max"}
+					class="number"
+					oninput={(e) => {
+						filters[column.name] = [
+							filters[column.name]?.[0] || null,
+							e.target.value,
+							true,
+						];
+					}}
+				/>
+			{:else}
+				<input
+					type="number"
+					placeholder={column.name}
+					oninput={(e) => {
+						if (e.target.value) {
+							filters[column.name] = [
+								e.target.value,
+								e.target.value,
+								filters[column.name]?.[2],
+							];
+						} else {
+							delete filters[column.name];
+						}
+					}}
+				/>
+			{/if}
+
+			<p>Range?</p>
+			<input
+				type="checkbox"
+				class="rangecheck"
+				oninput={(e) => {
+					filters[column.name] = [
+						null,
+						null,
+						filters[column.name]?.[2] ? false : true,
+					];
+				}}
+			/>
+		{:else if column.name === "name"}{:else if column.type === "text" && column.name !== "name"}
+			<details class="dropdown">
+				<summary>{column.name}</summary>
+				<ul>
+					{#await supabase.from(selectedTable).select(column.name) then values}
+						{#each [...new Set(values.data.map((val) => Object.keys(val).map((key) => val[key])[0]))] as value}
+							<li>
+								<label class="checkboxlabel">
+									<input
+										type="checkbox"
+										class="checkbox"
+										oninput={() => {
+											if (!filters[column.name]) filters[column.name] = {};
+											filters[column.name][value] = filters[column.name]?.[
+												value
+											]
+												? false
+												: true;
+										}}
+									/>
+									<p class="checkboxlabel">{value}</p></label
+								>
+							</li>
+						{/each}
+					{/await}
+				</ul>
+			</details>
+		{:else if column.name === "id"}{:else}
+			<input type="text" placeholder={column.name} />
+		{/if}
+	{/each}
+</div>
+
+<style lang="sass">
+
+#options
+  display: flex 
+  flex-direction: row
+  flex-columns: 2
+  flex-wrap: wrap
+  align-items: center
+input[type=number], input[type=text], select
+  width:12rem
+p:not(.checkboxlabel), select, input:not([type=checkbox]), button, h1, details
+  margin-top: 1rem
+  margin-left: 1rem
+label > p 
+  margin-bottom: -.5rem
+label.checkboxlabel 
+  display: flex
+  align-items: center
+.rangecheck
+  margin-left: 1rem 
+  margin-top: 0rem !important
+input[type=checkbox]
+  margin-top: .4rem
+
+</style>
--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -4,10 +4,10 @@
 	import { auth, supabase } from "$lib/supabase";
 	import type { SearchResponse } from "algoliasearch";
 	import Login from "$lib/components/login.svelte";
+	import Filters from "$lib/components/filters.svelte";
 	import NewColumn from "$lib/components/new_column.svelte";
 	let selectedTable = $state("");
 	let newTableName = $state("");
-	let filters = $state({});
 	let algoliaFilters = $state("");
 	let algoliaResults = $state<Promise<SearchResponse<unknown>>>();
 	let algoliaSearchText = $state("");
@@ -23,31 +23,6 @@
 			indexName: "stuff",
 			searchParams: { filters: algoliaFilters, query: algoliaSearchText },
 		});
-	});
-	$effect(() => {
-		let tempFilters = "";
-		Object.keys(filters).forEach(async (key) => {
-			const filter = filters[key];
-			if (filter[0]) {
-				tempFilters += `${key} >= ${filter[0]} AND `;
-			}
-			if (filter[1]) {
-				tempFilters += `${key} <= ${filter[1]} AND `;
-			}
-			// text filter
-			if (filter.constructor === Object) {
-				Object.keys(filter).forEach((k) => {
-					if (filter[k]) tempFilters += `${key}:'${k}' OR `;
-				});
-			}
-		});
-		// workaround to bypass the OR statement from text statements
-		if (selectedTable) {
-			if (tempFilters.split("OR").pop() == " ")
-				tempFilters = tempFilters.slice(0, -3) + "AND ";
-			tempFilters += `table: ${selectedTable}`;
-			algoliaFilters = tempFilters;
-		} else algoliaFilters = "";
 	});
 </script>
 
@@ -72,99 +47,7 @@
 			</div>
 		{/await}
 		{#await supabase.rpc("get_cols", { tablename: selectedTable }) then table}
-			<div id="options">
-				{#each table.data as column}
-					{#if column.type === "smallint"}
-						{#if filters[column.name]?.[2]}
-							<input
-								type="number"
-								placeholder={column.name + ": min"}
-								class="number"
-								oninput={(e) => {
-									filters[column.name] = [
-										e.target.value,
-										filters[column.name]?.[1] || null,
-										true,
-									];
-								}}
-							/>
-							<input
-								type="number"
-								placeholder={column.name + ": max"}
-								class="number"
-								oninput={(e) => {
-									filters[column.name] = [
-										filters[column.name]?.[0] || null,
-										e.target.value,
-										true,
-									];
-								}}
-							/>
-						{:else}
-							<input
-								type="number"
-								placeholder={column.name}
-								oninput={(e) => {
-									if (e.target.value) {
-										filters[column.name] = [
-											e.target.value,
-											e.target.value,
-											filters[column.name]?.[2],
-										];
-									} else {
-										delete filters[column.name];
-									}
-								}}
-							/>
-						{/if}
-
-						<p>Range?</p>
-						<input
-							type="checkbox"
-							class="rangecheck"
-							oninput={(e) => {
-								filters[column.name] = [
-									null,
-									null,
-									filters[column.name]?.[2] ? false : true,
-								];
-							}}
-						/>
-					{:else if column.name === "name"}{:else if column.type === "text" && column.name !== "name"}
-						<details class="dropdown">
-							<summary>{column.name}</summary>
-							<ul>
-								{#await supabase
-									.from(selectedTable)
-									.select(column.name) then values}
-									{#each [...new Set(values.data.map((val) => Object.keys(val).map((key) => val[key])[0]))] as value}
-										<li>
-											<label class="checkboxlabel">
-												<input
-													type="checkbox"
-													class="checkbox"
-													oninput={() => {
-														if (!filters[column.name])
-															filters[column.name] = {};
-														filters[column.name][value] = filters[
-															column.name
-														]?.[value]
-															? false
-															: true;
-													}}
-												/>
-												<p class="checkboxlabel">{value}</p></label
-											>
-										</li>
-									{/each}
-								{/await}
-							</ul>
-						</details>
-					{:else if column.name === "id"}{:else}
-						<input type="text" placeholder={column.name} />
-					{/if}
-				{/each}
-			</div>
+			<Filters {table} bind:algoliaFilters {selectedTable} />
 			<div class="searchdiv">
 				{#if selectedTable !== "New thing type"}
 					<input
@@ -228,19 +111,9 @@
   flex-wrap: wrap
 button
   margin-right: 1rem
-#options
-  display: flex 
-  flex-direction: row
-  flex-columns: 2
-  flex-wrap: wrap
-  align-items: center
-input[type=number], input[type=text], select
-  width:12rem
-p:not(.checkboxlabel), select, input:not([type=checkbox]), button, h1, details
-  margin-top: 1rem
-  margin-left: 1rem
 .header 
   display: flex
+  margin-top:1rem
   align-items: center
 .search 
   width: 21.5rem
@@ -249,12 +122,7 @@ p:not(.checkboxlabel), select, input:not([type=checkbox]), button, h1, details
   flex-direction: column
 label > p 
   margin-bottom: -.5rem
-label.checkboxlabel 
-  display: flex
-  align-items: center
-.rangecheck
-  margin-left: 1rem 
-  margin-top: 0rem !important
-input[type=checkbox]
-  margin-top: .4rem
+select 
+ width: 12rem
+ margin-left: 1rem
 </style>
