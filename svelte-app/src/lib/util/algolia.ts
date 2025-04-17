6fdbe8917598368e7c7f7e651a28884d07aeb647
zentag <38821230+zentag@users.noreply.github.com>
2025-04-17 12:05:40 -0400

    fix typo

--- svelte-app/src/lib/util/algolia.ts
+++ svelte-app/src/lib/util/algolia.ts
@@ -2,5 +2,5 @@ import { algoliasearch } from "algoliasearch";
 
 export const client = algoliasearch(
   import.meta.env.VITE_ALGOLIA_APP_ID,
-  import.meta.env.VITE_ALGOLIA_SEARCH_ONLY_API_KEY,
+  import.meta.env.VITE_ALGOLIA_SEARCH_ONLY_KEY,
 );
