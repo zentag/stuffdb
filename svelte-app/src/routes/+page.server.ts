b0dff626b905336c8e7b3560bde81be8629332e6
zentag <38821230+zentag@users.noreply.github.com>
2025-04-17 11:23:39 -0400

    move algolia update index to server-side component

---
+++ svelte-app/src/routes/+page.server.ts
@@ -0,0 +1,62 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { algoliasearch } from "algoliasearch";
+import { supabase } from "$lib/supabase";
+
+export const load: PageServerLoad = async ({ params }) => {
+  let client;
+  switch (import.meta.env.VITE_HOSTING_LOCATION) {
+    case "netlify":
+      client = algoliasearch(
+        import.meta.env.VITE_ALGOLIA_APP_ID,
+        process.env.ALGOLIA_ADMIN_KEY,
+      );
+      break;
+    case "local":
+      client = algoliasearch(
+        import.meta.env.VITE_ALGOLIA_APP_ID,
+        import.meta.env.VITE_ALGOLIA_ADMIN_KEY,
+      );
+      break;
+    default:
+      error(500, "Something went wrong");
+  }
+  const realTables = (await supabase.rpc("list_tables")).data;
+  // work around to upload after everything gets processed
+  const tables = [...realTables];
+  let algoliaObjects = [];
+  let facetAttributes = [];
+  tables.forEach(async (table, index, array) => {
+    const data = (await supabase.from(table.tablename).select()).data?.map(
+      (row) => {
+        facetAttributes = [...facetAttributes, ...Object.keys(row)];
+        return {
+          objectID: row.id,
+          ...row,
+          table: table.tablename,
+        };
+      },
+    );
+    if (!data) return;
+    algoliaObjects = [...algoliaObjects, ...data];
+    if (index === array.length - 1) {
+      await client.saveObjects({
+        indexName: "stuff",
+        objects: algoliaObjects,
+      });
+      const facetAttributesFiltered = [
+        "table",
+        ...new Set(
+          facetAttributes.filter((attr) => {
+            if (attr === "name" || attr === "id") return false;
+            else return true;
+          }),
+        ),
+      ];
+      await client.setSettings({
+        indexName: "stuff",
+        indexSettings: { attributesForFaceting: facetAttributesFiltered },
+      });
+    }
+  });
+};
