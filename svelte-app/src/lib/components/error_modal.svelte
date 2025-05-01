54b3c8f955d8d642067d004c732e27f6f95ec7fe
zentag <38821230+zentag@users.noreply.github.com>
2025-05-01 10:53:08 -0400

    add error dialog component

---
+++ svelte-app/src/lib/components/error_modal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let { dialogOpen = $bindable(), errorMsg } = $props();
+</script>
+
+<dialog open={dialogOpen}>
+	<article>
+		<header>
+			<button aria-label="Close" rel="prev" onclick={() => (dialogOpen = false)}
+			></button>
+			<p>
+				<strong>⚠️ Oh no!</strong>
+			</p>
+		</header>
+		<p>
+			{errorMsg}
+		</p>
+	</article>
+</dialog>
+
+<style lang="sass">
+button 
+  float: right
+</style>
